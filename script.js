d4a6086dd38c3247937645d5a087ee725b36de63
NurGames <nur.games.dev@gmail.com>
2025-03-20 12:38:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ function clearDisplay() {
     document.getElementById("display").value = "";
 }
 
+function deleteLast() {
+    let currentValue = document.getElementById("display").value;
+    document.getElementById("display").value = currentValue.slice(0, -1);
+}
+
 function calculateResult() {
     try {
         document.getElementById("display").value = eval(document.getElementById("display").value);
-    } catch (error) {
-        alert("Қате енгізу!");
+    } catch {
+        document.getElementById("display").value = "Error";
     }
 }
